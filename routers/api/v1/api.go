b81ca8111a165a0331eb931ce97ee42f79fa4fee
Adam Majer <amajer@suse.com>
2026-03-27 13:53:45 +0100

    Error message if target of reparent exists

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1183,7 +1183,7 @@ func Routes() *web.Router {
 					m.Post("/reject", repo.RejectTransfer)
 				}, reqToken())
 				m.Group("/reparent", func() {
-					m.Post("", bind(api.ReparentRepoOption{}), repo.Reparent)
+					m.Post("", reqOwner(), bind(api.ReparentRepoOption{}), repo.Reparent)
 					m.Post("/accept", repo.AcceptReparent)
 					m.Post("/reject", repo.RejectReparent)
 				}, reqToken())
